b5894ffa071f2b1dbfd6556193259daa4ecb034f
Christoph Maier <christoph.maier@ieee.org>
2025-08-31 08:51:53 +0200

    librelane/config.yaml src/dac2u128out4in.v : try to wire unused terminals to _unused. This doesn't work

--- src/dac2u128out4in.v
+++ src/dac2u128out4in.v
@@ -1,4 +1,4 @@
-// File: dac2u128out4in.sv
+// File: dac2u128out4in.v
 // SystemVerilog: 128 unit source DAC module digital placeholder
 // Yosys hints: blackbox + keep_hierarchy.
 (* blackbox *)
@@ -6,7 +6,14 @@ module dac2u128out4in (
     input [127:0] ON,
     input [127:0] ONB,
     input [3:0] EN,
-    input [3:0] ENB
+    input [3:0] ENB,
+    inout [1:0] VpcascP,
+    inout [1:0] VbiasP,
+    output Iout
 );
+
+// List all unused inputs to prevent warnings
+wire _unused = &{VbiasP[1:0],VpcascP[1:0],Iout};
+
 
 endmodule
