88dc43dc7e7746a4c9a17ed58fe62a7446239e71
Christoph Maier <christoph.maier@ieee.org>
2025-09-03 00:20:13 +0200

    src/heichips25_pudding.sv inverterpair.sv : finally syntactically correct

--- src/heichips25_pudding.sv
+++ src/heichips25_pudding.sv
@@ -24,11 +24,19 @@ module heichips25_pudding(
     // List all unused inputs to prevent warnings
     wire _unused = &{ena, uio_in[7:0], ui_in[7:4]};
 
+
     logic datum, shift, transfer, dir;
 
-    logic[127:0] daisychain;
-    logic[127:0] state;
+    logic[127:0] daisychain, daisychainp, daisychainn;
+    logic[3:0] daisyen, daisyenp, daisyenn;
+    logic[127:0] state, statep, staten;
+    logic[3:0] stateen, stateenp, stateenn;
 
+    assign state = {128{ui_in[0]}};
+    assign daisychain = {128{ui_in[1]}};
+    assign stateen = {4{ui_in[2]}};
+    assign daisyen = {4{ui_in[3]}};
+/*
     assign datum    = ui_in[0];
     assign shift    = ui_in[1];
     assign transfer = ui_in[2];
@@ -56,28 +64,95 @@ module heichips25_pudding(
             end
         end
     end
-
+*/
     
 assign uo_out  = daisychain[127:120];
 assign uio_out = state[127:120];
 assign uio_oe  = 8'hFF;
 
+    digital128 digitalL (
+    .in(state[127:0]),
+    .outp(statep[127:0]),
+    .outn(staten[127:0])
+);
+
+    digital128 digitalH (
+    .in(daisychain[127:0]),
+    .outp(daisychainp[127:0]),
+    .outn(daisychainn[127:0])
+);
+
+    digital4 digitalenL (
+    .in(stateen[3:0]),
+    .outp(stateenp[3:0]),
+    .outn(stateenn[3:0])
+);
+
+    digital4 digitalenH (
+    .in(daisyen[3:0]),
+    .outp(daisyenp[3:0]),
+    .outn(daisyenn[3:0])
+);
+
 (* keep_hierarchy = "yes", keep = "yes" *) dac2u128out4in dacL (
-    .ON(state[127:0]),
-    .ONB(~state[127:0]),
-    .EN(ena),
-    .ENB(~ena),
+    .ON(statep[127:0]),
+    .ONB(staten[127:0]),
+    .EN(stateenp[3:0]),
+    .ENB(stateenn[3:0]),
     .VDD(VPWR),
     .VSS(VGND)
 );
 
 (* keep_hierarchy = "yes", keep = "yes" *) dac2u128out4in dacH (
-    .ON(daisychain[127:0]),
-    .ONB(~daisychain[127:0]),
-    .EN(ena),
-    .ENB(~ena),
+    .ON(daisychainp[127:0]),
+    .ONB(daisychainn[127:0]),
+    .EN(daisyenp[3:0]),
+    .ENB(daisyenn[3:0]),
     .VDD(VPWR),
     .VSS(VGND)
 );
 
+endmodule
+
+module digital128(
+    input  logic [127:0] in,
+    output logic [127:0] outn,
+    output logic [127:0] outp
+);
+  genvar i;
+    for (i = 0; i < 128; i++) begin : g
+        inverterpair u (
+            .IN   (in[i]),
+            .OUTN (outn[i]),
+            .OUTP (outp[i])
+        );
+  end
+endmodule
+
+module digital4(
+    input  logic [3:0] in,
+    output logic [3:0] outn,
+    output logic [3:0] outp
+);
+    // 4 instances, each bit wired to one instance
+  genvar i;
+      for (i = 0; i < 4; i++) begin : g
+          inverterpair u (
+              .IN   (in[i]),
+              .OUTN (outn[i]),
+              .OUTP (outp[i])
+          );
+  end
+endmodule
+
+// File: inverterpair.sv
+// SystemVerilog: simple ON - ON_N logic for the current mirrors
+
+module inverterpair (
+    input logic IN,
+    output logic OUTN,
+    output logic OUTP
+);
+(* keep *) sg13g2_inv_1 inv1 (.Y(OUTN), .A(IN));
+(* keep *) sg13g2_inv_1 inv2 (.Y(OUTP), .A(OUTN));
 endmodule
